5c99922bc8a39474383f178006343bf1df4826dc
RustyDotson <rustydotson97@gmail.com>
2020-01-04 22:23:54 -0500

    split get_prices into multiple functions. Also accounted for shipping in the average.

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def average_price(prices):
     for i in prices:
         overall_price += i
 
-    return overall_price/len(prices)
+    return overall_price / len(prices)
 
 
 def get(link):
@@ -44,36 +44,69 @@ def get(link):
     """
     page = requests.get(link).text
     fetch = bs(page, 'lxml')
+    return fetch
+
+
+def check_format(price):
+
+    if "," in price:
+        convert_price = ""
+        for j in price:
+            if j != ",":
+                convert_price = convert_price + j
+        price = convert_price
+
+    if " " in price:
+        price_range = price.split()
+        price = (float(price_range[0]) + float(price_range[-1][1:])) / 2
+
+    return price
+
+
+def check_shipping(ship_html):
+
+    if ship_html is None:
+        return False
+    elif str(ship_html.get_text()) == "Free Shipping":
+        return False
+    return True
+
+
+def add_shipping(price, ship_check):
+    shipping = ship_check.get_text()
+    temp_ship = shipping.split()
+    shipping = float(temp_ship[0][2:])
+    print(price)
+    price = float(price) + shipping
+    print(price)
+    return price
+
+
+def get_prices(page_data):
     price_list = []
 
     for i in range(199):
-        if fetch.find(id="srp-river-results-listing" + str(i + 1)) is None:
+        if page_data.find(id="srp-river-results-listing" + str(i + 1)) is None:
             return average_price(price_list)
 
-        games = fetch.find(id="srp-river-results-listing" + str(i + 1)).find(class_="s-item__price")
-        price = (games.get_text()[1:])
+        games = page_data.find(id="srp-river-results-listing" + str(i + 1)).find(class_="s-item__price")  # finds
+        # price of item
 
-        if "," in price:
-            convert_price = ""
-            for j in price:
-                if j != ",":
-                    convert_price = convert_price + j
-            price = convert_price
+        price = games.get_text()[1:]
+        price = check_format(price)
 
-        if " " in price:
-            price_range = price.split()
-            price = (float(price_range[0]) + float(price_range[-1][1:]))/2
+        ship_check = ((page_data.find(id="srp-river-results-listing" + str(i + 1)).find(
+            class_="s-item__shipping s-item__logisticsCost")))  # finds the shipping of item
 
-        #if float(price) > 1000.0:
-        #    continue
-        #else:
+        if check_shipping(ship_check) is True:
+            price = add_shipping(price, ship_check)
+
         price_list.append(float(price))
 
     return average_price(price_list)
 
 
 def main():
-
     game, console = get_names()
 
     url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + sub_space(game) + "+" + sub_space(console) + \
@@ -81,7 +114,9 @@ def main():
           "&_ipg=200"
 
     print(url)
-    average = get(url)
+    page = get(url)
+
+    average = get_prices(page)
 
     print("\nThe average price of " + game + " on the " + console + " is approximately $"
           + str("{0:.2f}".format(average)))  # Used to remove large floating decimal numbers in the average
